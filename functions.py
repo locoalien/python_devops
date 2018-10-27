1b19358e4bc3fc053bc4e9222042a3731b8fd754
santiago <santiagosk80@gmail.com>
2018-10-27 21:02:15 +1300

    development2

--- functions.py
+++ functions.py
@@ -1,2 +1,12 @@
 def sum_number(first_number, second_number):
     return first_number + second_number
+
+def get_full_name(firstname,lastname):
+    """ Nombre completo formato primer nombre, apellido
+
+    Argumentos de entrada:
+    Primer nombre: Ejemplo Santy
+    Apellido: Ejemplo Gonzalez
+    """
+    return lastname + "," + first_number
+
--- test_functions.py
+++ test_functions.py
@@ -10,6 +10,9 @@ class TestSum(unittest.TestCase):
 
     def test_sum_three(self):
         self.assertEqual(100, sum_number(50, 50))
+    
+    def test_full_name(self):
+        self.assertEqual("Santiago, Gonzalez", get_full_name("gonzalez","santiago"))
 
 if __name__ == '__main__':
     unittest.main()
