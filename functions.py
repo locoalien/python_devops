1e7b5f2f0cf2e42f1af77b4d5485f46ec724edd5
santiago <santiagosk80@gmail.com>
2018-10-27 21:21:33 +1300

    actualizacion

--- functions.py
+++ functions.py
@@ -2,11 +2,5 @@ def sum_number(first_number, second_number):
     return first_number + second_number
 
 def get_full_name(firstname, lastname):
-    """ Nombre completo formato primer nombre, apellido
-
-    Argumentos de entrada:
-    Primer nombre: Ejemplo Santy
-    Apellido: Ejemplo Gonzalez
-    """
     return lastname + ", " + firstname
 
